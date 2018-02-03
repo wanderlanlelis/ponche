0d4356e1ac809e41803b900a79471e587ae9b2e8
Wanderlan Lelis <wanderlanlelis@gmail.com>
2018-02-03 21:53:27 -0200

    Refatoração

--- db/query test/teste_movimentoInserir.sql
+++ db/query test/teste_movimentoInserir.sql
@@ -2,8 +2,7 @@
 USE mydb_ponche;
 CALL sp_movimentoApagarAll();
 
-CALL sp_movimentoInserir(
-	@descricao  	= 'Apartamento',
+SET @descricao  	= 'Apartamento',
 	@valor 			= 300.30,
 	@subcategoria 	= 1,
 	@usuario 		= 3,
@@ -12,6 +11,10 @@ CALL sp_movimentoInserir(
 	@parcela 		= 5,
     @conta 			= 1,
     @tipo 			= 2,
-    @vencimento 	= '2018-01-31');
+    @vencimento 	= '2018-01-31';
+CALL sp_movimentoInserir(
+	@descricao, @valor,@subcategoria,
+	@usuario,@dependente, @parcela, @conta, @tipo,
+	@vencimento, @recorrencia);
 
 SELECT * FROM vw_movimento;
